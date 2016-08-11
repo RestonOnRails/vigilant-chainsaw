7f152914d16412bff1d93acbcf850d178487443e
Dan Falcone <dafalcon@akamai.com>
2016-08-10 20:31:13 -0400

    Add twitter bootstrap and generate layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :boots
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "boots#index"
+  root to: 'boots#index'
 end
